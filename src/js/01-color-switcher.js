58211d1cd845c3cdee61b238d75d371391e60fe3
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-05-05 14:27:14 +0200

    background color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+function randomHexColor() {
+    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+};
+
+startBtn.addEventListener("click", () => {
+    startBtn.setAttribute('disabled', '')
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = randomHexColor();
+  }, 1000);
+    
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+});
 
